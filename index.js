0f6729ed10f6c63d15b7793b4dc232754843fab7
RaThomBoi <jessada.srm@gmail.com>
2022-09-22 23:21:28 +0700

    docs: delete unused variable.

--- index.js
+++ index.js
@@ -2,26 +2,9 @@ const { Deck } = require("./deck.js");
 const { CARD_VALUE_MAP } = require("./CARD_VALUE_MAP.js");
 const prompt = require("prompt-sync")({ sigint: true });
 
-// const CARD_VALUE_MAP = {
-// 	A: 1,
-// 	2: 2,
-// 	3: 3,
-// 	4: 4,
-// 	5: 5,
-// 	6: 6,
-// 	7: 7,
-// 	8: 8,
-// 	9: 9,
-// 	10: 0,
-// 	J: 0,
-// 	Q: 0,
-// 	K: 0,
-// };
-
 let playerCards, dealerCards, bet
 let totalEarn = 0;
 let isGameStart = true;
-
 
 startGame()
 function startGame(){
